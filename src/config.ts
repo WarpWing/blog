6cb4d04c37b36f6a75e679a6c1442704f26ac4db
Ty Chermsirivatana <professornitro@gmail.com>
2024-09-17 01:04:27 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -49,7 +49,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',
   name: 'Ty "WarpWing" Chermsirivatana',
-  bio: '19, he/him, Law and Policy Major w/ Computer Science Minor at Dickinson College',
+  bio: '19, he/him, Poli Sci & CS & Dana Research Assistant @ Dickinson College | DevOps @ SkyCrypt',
   links: [
     {
       name: 'GitHub',
